a04fd9dd928ef23acb24a8cf4fea4ad1c1148dea
JayJohnsonCw <cw_93@hotmail.com>
2017-04-09 20:31:52 +0800

    Loading screen version 2
    1. Added progress bar
    2. "myprogressbar.xml" is the file to put in drawable directory

--- MainActivity.java
+++ MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.tanchian.myapplication;
 
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public Button button;
+    //public Button button;
+    private ProgressBar progressBar;
+    private int progressStatus = 0;
+    private TextView textView;
+    private Handler handler = new Handler();
 
-        public void init () {
-            button=(Button)findViewById(R.id.button);
+      /*  public void init () {
+          button=(Button)findViewById(R.id.button);
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -21,12 +29,37 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(login);
                 }
             });
-        }
+        }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init();
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        textView = (TextView) findViewById(R.id.textView);
+
+        progressStatus = 0;
+        new Thread(new Runnable(){
+            public void run(){
+                while (progressStatus < 100){
+                    progressStatus += 5;
+                    handler.post (new Runnable(){
+                        public void run(){
+                            progressBar.setProgress(progressStatus);
+                            textView.setText("Loading: " + progressStatus + "/" + progressBar.getMax());
+                        }
+                    });
+                    try {
+                        Thread.sleep(200);
+                    }
+                    catch(InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+                Intent loading = new Intent(MainActivity.this, LoginActivity.class);
+
+                startActivity(loading);
+            }
+        }).start();
     }
 }
