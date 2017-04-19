285524f5c632384f5a97a1c75faf9ba67158bd2c
JayJohnsonCw <cw_93@hotmail.com>
2017-04-19 12:47:34 +0800

    Add files via upload

---
+++ ViewPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.example.tanchian.myapplication;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.v13.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private ArrayList<Fragment> mFragmentList = new ArrayList<>();
+    private ArrayList<String> mFragmentListTitles = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment, String title){
+        mFragmentList.add(fragment);
+        mFragmentListTitles.add(title);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentListTitles.get(position);
+    }
+}
